67f90aebf4b2e089e2500a0c10c81d6855124e64
9434003 <9434003@ceit.local>
2019-10-31 16:30:29 +0330

    test version of backend is implemented

---
+++ Backend/Server/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, jsonify
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+
+# App config.
+DEBUG = True
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+output_search_res = [
+    {"title": "milad", "description": ",سپاه پاسداران جمهوری اسلامی ایران سپاه پاسداران جمهوری اسلامی ایران ",
+     "img": "http://google.com"},
+    {"title": "arad", "description": "خرمشهرررررر آزاد شددددددد ",
+     "img": "http://google.me"},
+    {"title": "alireza", "description": "قیمت دلااررر کاهش یافتتتتتتتت ",
+     "img": "http://google.you"},
+    {"title": "mehdi", "description": ",دستگیری دو عامل دزدی در بازار تهران ",
+     "img": "http://google.him"}
+]
+
+output_news_content = {"title": "پیروزی انقلاب یمن",
+                       "description": ",سپاه پاسداران جمهوری اسلامی ایران سپاه پاسداران جمهوری اسلامی ایران ",
+                       "img": "http://google.com",
+                       "content": "میووویصصشیشصیشصییمیووویصصشیشصیشصییمیووویصصشیشصیشصییمیووویصصشیشصیشصییمیووویصصشیشصیشصییمیووویصصشیشصیشصییمیووویصصشیشصیشصییمیووویصصشیشصیشصییمیووویصصشیشصیشصییمیووویصصشیشصیشصییمیووویصصشیشصیشصییمیووویصصشیشصیشصییمیووویصصشیشصیشصییمیووویصصشیشصیشصییمیووویصصشیشصیشصییمیووویصصشیشصیشصییمیووویصصشیشصیشصییمیووویصصشیشصیشصییمیووویصصشیشصیشصییمیووویصصشیشصیشصییمیووویصصشیشصیشصییمیووویصصشیشصیشصییمیووویصصشیشصیشصییمیووویصصشیشصیشصییمیووویصصشیشصیشصییوووووووو"},
+
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+
+@app.route("/search", methods=['GET'])
+def search():
+    # todo: search query method in BTree must be called here and results !
+    results = output_search_res
+    return jsonify({"news_headers": results})
+
+
+@app.route("/news/<int:news_id>", methods=['GET'])
+def get_news(news_id):
+    results = output_news_content
+    return jsonify(results)
+
+
+if __name__ == "__main__":
+    app.run()
