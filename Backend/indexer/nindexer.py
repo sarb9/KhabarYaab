96a2c0703625a1041951d050d9ba7addb955fecf
9434003 <9434003@ceit.local>
2019-11-06 17:57:13 +0330

    dictionary storing and retrieving are implemented

--- Backend/indexer/nindexer.py
+++ Backend/indexer/nindexer.py
@@ -1,5 +1,6 @@
 from ling_modules import lemmatizer, normalizer, pipline, stemmer, tokenizer
 from models.news_model import NewsModel
+import pickle, os.path
 from dictionary.posting import Posting
 from collections import namedtuple
 from dictionary import dictionary, posting
@@ -22,7 +23,18 @@ class Indexer:
             for i, term in enumerate(tokens):
                 self.index.append(Occurance(term, Posting(model.id, i)))
 
-    def create_dictionary(self):
+    def create_dictionary(self, from_scratch):
+
+        def save_dictionary(dct):
+            with open('data/dictionary_obj.pkl', 'wb') as output:
+                pickle.dump(dct, output, pickle.HIGHEST_PROTOCOL)
+
+        def load_dictionary():
+            with open('data/dictionary_obj.pkl', 'rb') as input_file:
+                return pickle.load(input_file)
+
+        if not from_scratch and os.path.exists('data/dictionary_obj.pkl'):
+            return load_dictionary()
         self.index.sort()
         dct = dictionary.Dictionary()
 
@@ -47,5 +59,5 @@ class Indexer:
         # add last word to the dictionary
         poslist.df = poslist.df
         dct[prev] = poslist
-
+        save_dictionary(dct)
         return dct
