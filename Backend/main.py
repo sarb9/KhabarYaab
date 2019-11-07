a78a4c7b7a806f389a82e7e4fb0d59a61685843f
9434003 <9434003@ceit.local>
2019-11-07 14:11:59 +0330

    selected_parts is handled

--- Backend/main.py
+++ Backend/main.py
@@ -19,15 +19,78 @@ def get_news_content(id):
 
 def get_news_headers(query):
     print("your query: ", query)
+    query_phrases = qh.extract_query_parts(query, without_pipeline=True)
+    print(query_phrases, " mioooo")
     ans = qh.ask(query)
     results = []
-    for id in ans:
-        news_model_view = mdls[id]
+    for doc_id in ans:
+        news_model_view = mdls[doc_id]
+        selected_part = highlight_phrases_in_content(news_model_view.content, query_phrases)
         results.append(
-            {"id": news_model_view.id, "thumbnail": news_model_view.thumbnail, "title": news_model_view.title,
+            {"selected_parts": selected_part, "id": news_model_view.id, "thumbnail": news_model_view.thumbnail,
+             "title": news_model_view.title,
              "summary": news_model_view.summary, "publish_date": news_model_view.publish_date})
 
     return results
+
+
+def highlight_phrases_in_content(content, query_phrases):
+    result = ""
+
+    def get_lower_bound(index):
+        if index - threshold < 0:
+            return 0
+        else:
+            return index - threshold
+
+    def get_upper_bound(index):
+        if index + threshold > len(content):
+            return len(content)
+        else:
+            return index + threshold
+
+    highlighted_content = content
+
+    def bold_phrases(highlighted_content):
+        phrases = []
+        for qp in query_phrases:
+            if qp.b:
+                for term in qp.terms:
+                    phrases.append(term)
+        for phrase in phrases:
+            highlighted_content = highlighted_content.replace(phrase, "<b>" + phrase + "</b>")
+        return highlighted_content, phrases
+
+    highlighted_content, phrases = bold_phrases(highlighted_content)
+
+    threshold = 40 - 4 * len(phrases)
+    print(threshold, "threshold")
+    if threshold < 20:
+        threshold = 20
+
+    list_of_index = []
+    for phrase in phrases:
+        list_of_index.append(highlighted_content.find(phrase))
+    list_of_index.sort()
+    upper_index = 0
+    lower_index = 0
+    prev_index = None
+    done =False
+    print(list_of_index, " list of indexxx")
+    for index in list_of_index:
+        done = False
+        if prev_index is None:
+            lower_index = get_lower_bound(index)
+        elif index - prev_index > threshold:
+            done= True
+            result += highlighted_content[lower_index: upper_index] + " ... "
+            lower_index = get_lower_bound(index)
+        upper_index = get_upper_bound(index)
+        prev_index = index
+    if not done:
+        result += highlighted_content[lower_index: upper_index]
+    print("resultssss: ", result)
+    return result
 
 
 corpus = import_utils.load_corpus()
